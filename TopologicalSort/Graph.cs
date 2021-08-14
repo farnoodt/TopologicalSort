5d3591752b41471d714928707b87db186e17d5ab
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-14 14:36:22 -0700

    Add project files.

---
+++ TopologicalSort/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopologicalSort
+{
+    class Graph
+    {
+        public List<List<int>> adj = new List<List<int>>();
+        public int Nodes;
+
+        public Graph(int V)
+        {
+            this.Nodes = V;
+            for (int i = 0; i < V; i++)
+                adj.Add(new List<int>());
+        }
+
+        public void AddEdge(int Source, int Destination)
+        {
+            adj[Source].Add(Destination);
+        }
+
+        public void Sort()
+        {
+            bool[] Visited = new bool[Nodes];
+            Stack<int> S = new Stack<int>();
+
+            for(int i = 0; i< Nodes; i++)
+            {
+                if (!Visited[i])
+                    SortUtil(i, Visited, S);
+            }
+
+            Console.WriteLine(string.Join(" , ", S));
+            Console.ReadLine();
+        }
+
+        public void SortUtil(int startNode, bool[] Visited, Stack<int> Q)
+        {
+            Visited[startNode] = true;
+            List<int> node = new List<int>();
+            node = adj[startNode]; 
+
+            foreach(var child in node)
+            {
+                if (!Visited[child])
+                {
+                    SortUtil(child, Visited, Q);
+                    
+                }
+            }
+            Q.Push(startNode);
+        }
+    }
+}
---
+++ TopologicalSort/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TopologicalSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph(6);
+            g.AddEdge(5, 2);
+            g.AddEdge(5, 0);
+            g.AddEdge(4, 0);
+            g.AddEdge(4, 1);
+            g.AddEdge(2, 3);
+            g.AddEdge(3, 1);
+
+            g.Sort();
+            Console.ReadLine();
+        }
+    }
+}
